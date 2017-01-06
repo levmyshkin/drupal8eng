31c1f38a4bc1cb6e524a35072ec9b0e48d104be5
Ivan Abramenko <levmyshkin89@gmail.com>
2017-01-06 11:00:58 +0600

    Update Vendor dependencies

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -16,4 +16,5 @@ return array(
     '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
     '5255c38a0faeba867671b61dfda6d864' => $vendorDir . '/paragonie/random_compat/lib/random.php',
     'def43f6c87e4f8dfd0c9e1b1bab14fe8' => $vendorDir . '/symfony/polyfill-iconv/bootstrap.php',
+    '2c102faa651ef8ea5874edb585946bce' => $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
 );
